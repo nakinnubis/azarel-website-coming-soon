4d1be5c23d2b262587b0d63b239ea8db3823f69f
Vanessa Okeke <vanneka.okeke@gmail.com>
2022-03-02 19:37:37 -0600

    signup form build

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,9 +1,3 @@
-// to add email (edited)
-// POST 192.248.153.201:9011/api/v1/newsletter/:email 
-
-// to verify status
-// GET 192.248.153.201:9011/api/v1/newsletter/:email/exists 
-
 // submit header form
 let headerform = document.querySelector('#header-form')
 let sectionform = document.querySelector('#section-form')
@@ -13,33 +7,59 @@ const validateEmail = (emailval) => {
     return emailPattern.test(emailval);
 }
 
-console.log("hello")
+const emailcheck = (emailaddress) => {
+    let addemail = `http://192.248.153.201:9011/api/v1/newsletter/${emailaddress}`
+    let emailexists = `http://192.248.153.201:9011/api/v1/newsletter/${emailaddress}/exists`
+
+    let options = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    fetch(emailexists)
+        .then(response => {
+            return response.json()
+        })
+        .then(result => {
+            let isemailInDb = result.data
+            if (isemailInDb) {
+                return alert("This email already exists in the database")
+            } else {
+                fetch(addemail, options)
+                    .then(req => { return req.json() })
+                    .then(res => {
+                        alert("Congratulations! You're signed in and ready to go")
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
 
 headerform.addEventListener('submit', (e) => {
     e.preventDefault()
     let emailinput = document.querySelector('#header-input-form').value
 
-    let options = {
-        method: 'POST',
-        body: JSON.stringify(emailinput),
-        headers: {
-            "Content-Type":"application/json"
-        }
-    }
-
     if (!validateEmail(emailinput)) {
-        return console.log("Please enter a valid email")
+        return alert("Please enter a valid email")
     } else {
-        let url = `192.248.153.201:9011/api/v1/newsletter/${emailinput}`
-        fetch(url, options)
-            .then(response => {
-                return response.json()
-            })
-            .then(result => {
-                console.log(result)
-            })
-            .catch(error => {
-                console.log(error)
-            })
+        emailcheck(emailinput)
+    }
+})
+
+sectionform.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let sectionemailinput = document.querySelector('#section-input-form').value
+
+    if (!validateEmail(sectionemailinput)) {
+        return alert("Please enter a valid email")
+    } else {
+        emailcheck(sectionemailinput)
     }
 })
